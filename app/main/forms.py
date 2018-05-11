53e3c6c1c4f0deffba6f9fd7e6572fe719be5333
gxywl <gxywl@163.com>
2018-05-11 17:10:28 +0800

    上传--文件名问题还没解决

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 # _*_ encoding: utf-8 _*_
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, SelectField
+from wtforms import StringField, SubmitField, IntegerField, SelectField, FileField
 from wtforms.validators import DataRequired, InputRequired
 from ..models import Zytype
 
@@ -8,6 +8,7 @@ from ..models import Zytype
 class NameForm(FlaskForm):
     # xiangmu = StringField("布放项目", validators=[DataRequired()])
     # xiangmu = SelectField('布放项目', validators=[DataRequired()] , choices=[('0', '放生'),('1', '火施'),('2', '其他')])
+    uploadfile = FileField('选择文件', validators=[DataRequired()])
     zytype = SelectField('资源类型', validators=[DataRequired()], coerce=int)
     zyname = StringField("资源名称", validators=[DataRequired()])
     submit = SubmitField('记录')
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,8 @@
 # _*_ encoding: utf-8 _*_
+import hashlib
 import os
+import time
+import datetime
 
 from flask import redirect, url_for, render_template, flash, send_from_directory, request, abort
 from flask_login import current_user, login_required
@@ -15,14 +18,14 @@ from . import main
 @login_required
 def download(filename):
     if request.method == "GET":
-        #if os.path.isfile(os.path.join('upload', filename)):
+        # if os.path.isfile(os.path.join('upload', filename)):
         return send_from_directory('upload', filename, as_attachment=True)
-        #abort(404)
+        # abort(404)
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/upload', methods=['GET', 'POST'])
 @login_required
-def index():
+def upload():
     form = NameForm()
     # bujuan=None
     # jxsources = Jxsource.query.filter_by(user=current_user._get_current_object()).order_by(Jxsource.outtime.desc())
@@ -30,19 +33,73 @@ def index():
     user = current_user._get_current_object()
     if form.validate_on_submit():
         zytype = Zytype.query.get(form.zytype.data)
+        zyname = form.zyname.data
 
-        jxsource = Jxsource(user=user, zytype=zytype, zyname=form.zyname.data)
+        uploaded_file = form.uploadfile.data
+
+        # filename = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        # filename = os.path.join('upload', uploaded_file.filename)
+
+        # uploaddir = os.path.join(os.path.dirname(__file__), 'static', 'upload')
+        pdir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+        # uploaddir = os.path.join(os.path.dirname(__file__), 'static', 'upload')
+        uploaddir = os.path.join(pdir, 'upload')
+
+        # filename = os.path.join(uploaddir, 'tmpabc.txt')
+
+        ext = os.path.splitext(uploaded_file.filename)[1]
+
+        t = time.time()
+        nowTime = lambda: int(round(t * 1000))
+
+        tmpstr = current_user.username + str(nowTime())
+
+        tmpstr = tmpstr.encode("utf-8")
+        md5filename = hashlib.md5()
+        #md5filename.update(tmpstr)
+
+        md5filename.update(tmpstr.encode('utf-8'))
+
+
+        savefilename = md5filename[:15] + ext  # uploaded_file.filename
+
+        fullsavefilename = os.path.join(uploaddir, savefilename)
+        uploaded_file.save(fullsavefilename)
+
+        jxsource = Jxsource(user=user, zytype=zytype, zyname=zyname, filename=savefilename)
+
+        # jxsource = Jxsource(user=user, zytype=zytype, zyname=zyname, filename='temp')
+
         db.session.add(jxsource)
-        flash('添加成功')
+        flash('上传成功')
         return redirect(url_for('.index'))
+    else:
+        return render_template('upload.html', form=form, jxsources=jxsources)
+
+
+@main.route('/', methods=['GET'])
+@login_required
+def index():
+    # form = NameForm()
+    # bujuan=None
+    # jxsources = Jxsource.query.filter_by(user=current_user._get_current_object()).order_by(Jxsource.outtime.desc())
+    jxsources = Jxsource.query.order_by(Jxsource.uptime.desc())
+    user = current_user._get_current_object()
+    # if form.validate_on_submit():
+    #     zytype = Zytype.query.get(form.zytype.data)
+    #
+    #     jxsource = Jxsource(user=user, zytype=zytype, zyname=form.zyname.data)
+    #     db.session.add(jxsource)
+    #     flash('添加成功')
+    #     return redirect(url_for('.index'))
 
     # 管理账号转到管理页
     if user.isadm:
         return redirect(url_for('main.count'))
     else:
         # 一般用户转转到首页..
-        return render_template('index.html', form=form, jxsources=jxsources)
-        # return render_template('index.html', jxsources=jxsources)
+        # return render_template('index.html', form=form, jxsources=jxsources)
+        return render_template('index.html', jxsources=jxsources)
 
 #
 # @main.route('/list')
