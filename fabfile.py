073e199bb9c02182617ac307b2237ee24393d4db
gxywl <gxywl@163.com>
2018-05-12 12:06:59 +0800

    加入了fabfile自动布置

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,14 @@
+# _*_ encoding: utf-8 _*_
+
+'''
+pip freeze > requirements.txt
+
+'''
+
 from fabric.api import local, env, run ,sudo ,cd
 
-env.hosts=['ubuntu@192.168.1.14']
+env.hosts=['deploy@192.168.1.9']
+env.password='pub'
 
 def test():
     local('python -m unittest discover')
@@ -39,26 +47,37 @@ def setup():
     #
     # run("git config --global credential.helper store")
 
-    with cd("/home/deploy/"):
-        run("git clone https://github.com/gxywl/jxzyk.git")
+    with cd("/home/deploy/websites/"):
+        sudo("git clone https://github.com/gxywl/jxzyk.git")
 
-    with cd("/home/deploy/jxzyk"):
-        sudo("virtualenv venv")
-        sudo("source venv/bin/active")
+    with cd("/home/deploy/websites/jxzyk"):
+        # sudo("virtualenv venv")
+        # sudo("source venv/bin/active")
+        sudo("virtualenv /home/deploy/websites/jxzyk/venv")
+        sudo(". /home/deploy/websites/jxzyk/venv/bin/activate")
+        '''
+        #sudo('nano /home/deploy/websites/jxzyk/venv/lib/python2.7/site.py')
+        # _*_ encoding: utf-8 _*_
+        # encoding = "ascii" ==> "utf8"
+        '''
 
 
-        run("/home/deploy/jxzyk/venv/bin/pip install -r requirements.txt")
+        # run("/home/deploy/jxzyk/venv/bin/pip install -r requirements.txt")
+        sudo("/home/deploy/websites/jxzyk/venv/bin/pip install -r /home/deploy/websites/jxzyk/requirements.txt -i https://pypi.douban.com/simple")  #及uwsgi
+
     #     run("python manage.py createdb")
-        run("/home/deploy/jxzyk/venv/bin/python manage.py db upgrade")
+        sudo("/home/deploy/websites/jxzyk/venv/bin/python manage.py db upgrade")
 
-        sudo("/home/deploy/jxzyk/venv/bin/pip  install  uwsgi")
-        sudo("/home/deploy/jxzyk/venv/bin/uwsgi --ini /home/deploy/jxzyk/uwsgi.ini")
+        sudo("/home/deploy/websites/jxzyk/venv/bin/pip  install  uwsgi")
+        sudo("/home/deploy/websites/jxzyk/venv/bin/uwsgi --ini /home/deploy/websites/jxzyk/uwsgi.ini")
 
         sudo ("cp superivsord.conf /etc/supervisor/conf.d/jxzyk.conf")
 
-        sudo("cp nginx.conf /etc/nginx/sites-available/j.conf")
-        sudo("ln -s /etc/nginx/sites-available/j.conf /etc/nginx/sites-enabled/j.conf ")
+        sudo("cp nginx.conf /etc/nginx/sites-available/jx.conf")
+        sudo("ln -s /etc/nginx/sites-available/j.conf /etc/nginx/sites-enabled/jx.conf ")
 
+    sudo('chown -R www-data:www-data /home/deploy/websites/jxzyk')
+    sudo('chmod -R 755 /home/deploy/websites/jxzyk')
 
     sudo(('service supervisor restart'))
     sudo(('service nginx restart'))
@@ -67,7 +86,7 @@ def deploy():
     # test()
     # upgrade_libs()
 
-    with cd('/home/deploy/jxzyk'):
-        run("git pull")
-        run("pip install -r requirements.txt")
-        run("python manage.py db upgrade")
+    with cd('/home/deploy/websites/jxzyk'):
+        sudo("git pull")
+        sudo("/home/deploy/websites/jxzyk/venv/bipip install -r requirements.txt")
+        sudo("/home/deploy/websites/jxzyk/venv/bipython manage.py db upgrade")
